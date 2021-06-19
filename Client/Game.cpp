a29bbb66f3bf3d45612ff2dd550484b661bd152f
NamTaeHyun <nth0310@naver.com>
2021-06-19 09:58:16 +0900

    +IndexBuffer -> Convert Triangle to Rectangle

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -9,15 +9,46 @@ void Game::Init(const WindowInfo& info)
 {
 	GEngine->Init(info);
 
-	vector<Vertex> vec(3);
-	vec[0].pos		= Vec3(0.f, 0.5f, 0.5f);
-	vec[0].color	= Vec4(1.f, 0.f, 0.f, 1.f);
-	vec[1].pos		= Vec3(0.5f, -0.5f, 0.5f);
-	vec[1].color	= Vec4(0.f, 1.0f, 0.f, 1.f);
-	vec[2].pos		= Vec3(-0.5f, -0.5f, 0.5f);
-	vec[2].color	= Vec4(0.f, 0.f, 1.0f,1.f);
-	mesh->Init(vec);
+	//vector<Vertex> vec(6);
+	//vec[0].pos		= Vec3(-0.5f, 0.5f, 0.5f);
+	//vec[0].color	= Vec4(1.f, 0.f, 0.f, 1.f);
+	//vec[1].pos		= Vec3(0.5f, 0.5f, 0.5f);
+	//vec[1].color	= Vec4(0.f, 1.0f, 0.f, 1.f);
+	//vec[2].pos		= Vec3(0.5f, -0.5f, 0.5f);
+	//vec[2].color	= Vec4(0.f, 0.f, 1.0f,1.f);
+	//
+	//vec[3].pos = Vec3(0.5f, -0.5f, 0.5f);
+	//vec[3].color = Vec4(1.f, 0.f, 0.f, 1.f);
+	//vec[4].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	//vec[4].color = Vec4(0.f, 1.0f, 0.f, 1.f);
+	//vec[5].pos = Vec3(-0.5f, 0.5f, 0.5f);
+	//vec[5].color = Vec4(0.f, 0.f, 1.0f, 1.f);
 
+	vector<Vertex> vec(4);
+	vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+	vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+	vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
+
+
+	vector<uint32> indexVec;
+	{
+		indexVec.emplace_back(0);
+		indexVec.emplace_back(1);
+		indexVec.emplace_back(2);
+	}
+	{
+		indexVec.emplace_back(0);
+		indexVec.emplace_back(2);
+		indexVec.emplace_back(3);
+	}
+
+	mesh->Init(vec,indexVec);
+	
 	shader->Init(L"..\\Resources\\Shader\\default.hlsli");
 
 	GEngine->GetCmdQueue()->WaitSync();
@@ -28,17 +59,9 @@ void Game::Update()
 	GEngine->RenderBegin();
 	
 	shader->Update();
-//	GEngine->Render();
 	{
 		Transform t;
-		t.offset = Vec4(0.75f, 0.f, 0.f, 0.f);
-		mesh->SetTransform(t);
-
-		mesh->Render();
-	}
-	{
-		Transform t;
-		t.offset = Vec4(0.f, 0.75f, 0.f, 0.f);
+		t.offset = Vec4(0.f, 0.f, 0.f, 0.f);
 		mesh->SetTransform(t);
 
 		mesh->Render();
--- Engine/Mesh.cpp
+++ Engine/Mesh.cpp
@@ -2,13 +2,39 @@
 #include "Mesh.h"
 #include "Engine.h"
 
-void Mesh::Init(vector<Vertex>& vec)
+void Mesh::Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexbuffer)
 {
-	_vertexCount = static_cast<uint32>(vec.size());
-	uint32 bufferSize = _vertexCount * sizeof(Vertex);	
+	CreateVertexBuffer(vertexBuffer);
+	CreateIndexBuffer(indexbuffer);
+}
 
-	D3D12_HEAP_PROPERTIES heapProperty	= CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
-	D3D12_RESOURCE_DESC desc					= CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
+void Mesh::Render()
+{
+	CMD_LIST->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);	// 정점 연결 형식(삼각형)
+	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView);								// Slot: (0~15)
+	CMD_LIST->IASetIndexBuffer(&_indexBufferView);
+
+	// TODO 인자 셋팅
+	// 1) Buffer에 데이터 셋팅
+	// 2) TableDescHeap에 CBV전달
+	// 3) 모두 세팅이 끝났으면 TableDescHeap 커밋
+	{
+		D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetConstantBuffer()->PushData(0, &_transform, sizeof(_transform));
+		GEngine->GetTableDescriptorHeap()->SetCBV(handle, CBV_REGISTER::b0);
+	}
+	GEngine->GetTableDescriptorHeap()->CommitTable();
+
+	//	CMD_LIST->DrawInstanced(_vertexCount, 1, 0, 0);	// Vertex를 이용한 버전
+	CMD_LIST->DrawIndexedInstanced(_indexCount, 1, 0, 0, 0);
+}
+
+void Mesh::CreateVertexBuffer(const vector<Vertex>& buffer)
+{
+	_vertexCount = static_cast<uint32>(buffer.size());
+	uint32 bufferSize = _vertexCount * sizeof(Vertex);
+
+	D3D12_HEAP_PROPERTIES heapProperty = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
 
 	DEVICE->CreateCommittedResource(
 		&heapProperty,
@@ -23,7 +49,7 @@ void Mesh::Init(vector<Vertex>& vec)
 	void* vertexDataBuffer = nullptr;
 	CD3DX12_RANGE readRange(0, 0); // We do not intend to read from this resource on the CPU.
 	_vertexBuffer->Map(0, &readRange, &vertexDataBuffer);		// 뚜껑을 열어서
-	::memcpy(vertexDataBuffer, &vec[0], bufferSize);				// GPU공간에 데이터를 밀어넣고
+	::memcpy(vertexDataBuffer, &buffer[0], bufferSize);				// GPU공간에 데이터를 밀어넣고
 	_vertexBuffer->Unmap(0, nullptr);								// 뚜껑을 닫음.
 	// -> 이렇게 하면 GPU에 데이터 전달 완료
 
@@ -35,24 +61,29 @@ void Mesh::Init(vector<Vertex>& vec)
 	_vertexBufferView.SizeInBytes = bufferSize; // 버퍼의 크기	
 }
 
-void Mesh::Render()
+void Mesh::CreateIndexBuffer(const vector<uint32>& buffer)
 {
-	CMD_LIST->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);	// 정점 연결 형식(삼각형)
-	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView);								// Slot: (0~15)
+	_indexCount = static_cast<uint32>(buffer.size());
+	uint32 bufferSize = _indexCount * sizeof(uint32);
 
-	// TODO 인자 셋팅
-	// 1) Buffer에 데이터 셋팅
-	// 2) TableDescHeap에 CBV전달
-	// 3) 모두 세팅이 끝났으면 TableDescHeap 커밋
-	{
-		D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetConstantBuffer()->PushData(0, &_transform, sizeof(_transform));
-		GEngine->GetTableDescriptorHeap()->SetCBV(handle, CBV_REGISTER::b0);
-	}
-	{
-		D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetConstantBuffer()->PushData(0, &_transform, sizeof(_transform));
-		GEngine->GetTableDescriptorHeap()->SetCBV(handle, CBV_REGISTER::b1);
-	}
-	GEngine->GetTableDescriptorHeap()->CommitTable();
+	D3D12_HEAP_PROPERTIES heapProperty = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
 
-	CMD_LIST->DrawInstanced(_vertexCount, 1, 0, 0);
+	DEVICE->CreateCommittedResource(
+		&heapProperty,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&_indexBuffer));
+
+	void* indexDataBuffer = nullptr;
+	CD3DX12_RANGE readRange(0, 0); // We do not intend to read from this resource on the CPU.
+	_indexBuffer->Map(0, &readRange, &indexDataBuffer);
+	::memcpy(indexDataBuffer, &buffer[0], bufferSize);
+	_indexBuffer->Unmap(0, nullptr);
+
+	_indexBufferView.BufferLocation = _indexBuffer->GetGPUVirtualAddress();
+	_indexBufferView.Format = DXGI_FORMAT_R32_UINT;
+	_indexBufferView.SizeInBytes = bufferSize;
 }
