3d9993e440771243b16bcfec8355d40c1fed78a6
NamTaeHyun <nth0310@naver.com>
2021-06-21 22:33:47 +0900

    add SceneManager

---
+++ Engine/Scene.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "Scene.h"
+#include "GameObject.h"
+
+void Scene::Awake()
+{
+	for (const shared_ptr<GameObject>& gameObject : _gameObjects)			// const로 놓은 이유는, shared_ptr의 경우 
+																								// const를 안하면, refCount를 증가시켜버린다. 
+																								// 성능 저하 방지
+	{
+		gameObject->Awake();
+	}
+}
+
+void Scene::Start()
+{
+	for (const shared_ptr<GameObject>& gameObject : _gameObjects)
+	{
+		gameObject->Start();
+	}
+}
+
+void Scene::Update()
+{
+	for (const shared_ptr<GameObject>& gameObject : _gameObjects)
+	{
+		gameObject->Update();
+	}
+}
+
+void Scene::LateUpdate()
+{
+	for (const shared_ptr<GameObject>& gameObject : _gameObjects)
+	{
+		gameObject->LateUpdate();
+	}
+}
+
+void Scene::AddGameObject(shared_ptr<GameObject> gameObject)
+{
+	_gameObjects.emplace_back(gameObject);
+}
+
+void Scene::RemoveGameObject(shared_ptr<GameObject> gameObject)
+{
+	auto findIt = std::find(_gameObjects.begin(), _gameObjects.end(), gameObject);
+
+	if (findIt != _gameObjects.end())
+	{
+		_gameObjects.erase(findIt);
+	}
+}
