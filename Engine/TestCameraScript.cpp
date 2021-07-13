55e804b0ef96356a54cc5c5fbc60aade53a44c87
NamTaeHyun <nth0310@naver.com>
2021-07-13 22:02:27 +0900

    Add Picking

--- Engine/TestCameraScript.cpp
+++ Engine/TestCameraScript.cpp
@@ -5,6 +5,7 @@
 #include "GameObject.h"
 #include "Input.h"
 #include "Timer.h"
+#include	"SceneManager.h"
 
 TestCameraScript::TestCameraScript()
 {
@@ -52,5 +53,10 @@ void TestCameraScript::LateUpdate()
 		GetTransform()->SetLocalRotation(rotation);
 	}
 
+	if (INPUT->GetButtonDown(KEY_TYPE::RBUTTON))
+	{
+		const POINT& pos = INPUT->GetMousePos();
+		GET_SINGLE(SceneManager)->Pick(pos.x, pos.y);
+	}
 	GetTransform()->SetLocalPosition(pos);
 }
